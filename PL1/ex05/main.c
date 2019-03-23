6089b2700fff4ec651e86ebd32bace090970b76f
Gonçalo Fonseca <1150503@isep.ipp.pt>
2019-03-23 19:44:57 +0000

    Ex04, 05 done and properly working :ambulance:

--- PL1/ex05/main.c
+++ PL1/ex05/main.c
@@ -6,12 +6,12 @@
 
 void f1(), f2();
 
-int main(){	
+int main(){
 	int i, status;
 
 	for(i = 0; i < 2; i++){
 		pid_t p = fork();
-	
+
 		if(p == 0){
 			if(i == 0)
 				f1();
@@ -19,26 +19,25 @@ int main(){
 				f2();
 		}
 	}
-	
+
 	for(i = 0; i < 2; i++){
 		wait(&status);
-		
+
 		if(WIFEXITED(status))
 			printf("%d\n", WEXITSTATUS(status));
 	}
-	
+
 	return 0;
 }
 
 void f1(){
 	sleep(1);
-	
+
 	exit(1);
 }
 
 void f2(){
 	sleep(2);
-	
+
 	exit(2);
 }
-	
